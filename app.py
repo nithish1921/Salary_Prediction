e7b7a7a745935b8ba1c6b907ef2324025cff283b
nithish1921 <210303126220@paruluniversity.ac.in>
2025-10-28 10:58:58 +0530

    Commit

--- app.py
+++ app.py
@@ -128,6 +128,5 @@ def index():
     return render_template("index.html", dropdowns=dropdowns)
 
 
-if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
